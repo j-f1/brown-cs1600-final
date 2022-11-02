486cb136ca56ab70b3bf6ce7d9d7c61270f16980
Mark Lavrentyev <lavrema@outlook.com>
2022-11-02 14:23:27 -0400

    Add some gpt-3 specific stuff

--- gpt3-requester/gpt3-requester.ino
+++ gpt3-requester/gpt3-requester.ino
@@ -9,8 +9,8 @@ WiFiClient wifiClient;
 HttpClient httpClient(wifiClient);
 
 // GPT-3 API setup
-//char host[] = "https://api.openai.com";
-//char endpoint[] = "/v1/completions";
+char openAiHostname[] = "https://api.openai.com";
+char completionsEndpoint[] = "/v1/completions";
 
 char host[] = "dummyjson.com";
 char endpoint[] = "/todos/1";
@@ -36,7 +36,12 @@ void loop() {
   int err = httpClient.get(host, endpoint);
 
   if (!err) {
-    Serial.println(httpClient.responseStatusCode());
+    if (httpClient.responseStatusCode() >= 200 && httpClient.responseStatusCode() <= 299) {
+      
+    } else {
+      Serial.print("Response returned a non-success status code: ");
+      Serial.println(httpClient.responseStatusCode());
+    }
   } else {
     Serial.print("Error code: ");
     Serial.println(err);
@@ -44,4 +49,56 @@ void loop() {
 
   httpClient.stop();
   delay(2500);
+}
+
+// ======================= GPT-3 calls ========================
+
+JSONVar createOpenAiApiRequest(String unvoweledWord) {
+  JSONVar requestPayload;
+
+  String basePrompt = "These words are missing their vowels. Please fill them in.\n  mpt -> empty\n  dtrrt -> deteriorate\n  dmrct -> demarcate\n  ";
+  String prompt = basePrompt + unvoweledWord.toLowerCase() + " ->";
+
+  requestPayload["model"] = "text-ada-001";
+  requestPayload["prompt"] = prompt;
+  requestPayload["max_tokens"] = "20";
+  requestPayload["temperature"] = 0;
+  requestPayload["stop"] = "\n";
+
+  return requestPayload;
+}
+
+JSONVar getTextCompletion() {
+  int err = httpClient.post(openAiHostname, completionsEndpoint);
+
+  if (!err) {
+    if (httpClient.responseStatusCode() >= 200 && httpClient.responseStatusCode() <= 299) {
+      Serial.println("Successful response received");
+      // TODO: handle payload here, see https://github.com/amcewen/HttpClient/blob/master/HttpClient.h
+    } else {
+      Serial.print("Response returned a non-success status code: ");
+      Serial.println(httpClient.responseStatusCode());
+    }
+  } else {
+    Serial.print("Request received error: ");
+    switch (err) {
+      case HTTP_ERROR_CONNECTION_FAILED:
+        Serial.println("connection failed (-1)");
+        break;
+      case HTTP_ERROR_API:
+        Serial.println("unexpected call (-2); HttpClient class used incorrectly.");
+        break;
+      case HTTP_ERROR_TIMED_OUT:
+        Serial.println("timed out (-3)");
+        break;
+      case HTTP_ERROR_INVALID_RESPONSE:
+        Serial.println("invalid response (-4); is it an HTTP server?");
+        break;
+      default:
+        Serial.print("unexpected error (");
+        Serial.print(err);
+        Serial.println(")");
+        break;
+    }
+  }
 }
