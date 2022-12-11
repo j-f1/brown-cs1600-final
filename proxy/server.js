cd330ed5961b40140a85cca7882cb06e3343e199
Jed Fox <git@jedfox.com>
2022-12-11 14:45:09 -0500

    server: error handling

--- proxy/server.js
+++ proxy/server.js
@@ -28,9 +28,11 @@ fastify.put("/", async function (request, reply) {
   if (!input) return reply.send("empty input!");
   if (!/^[a-z]+$/.test(input)) return reply.send("invalid input!");
 
-  const completion = await openai.createCompletion({
-    model,
-    prompt: `Output up to ten valid, complete English corrections for words which are missing their vowels. Only add the letters A, E, I, O, or Y. Do not add any additional consonants or other characters in the corrections.
+  let completion
+  try {
+    completion = await openai.createCompletion({
+      model,
+      prompt: `Output up to ten valid, complete English corrections for words which are missing their vowels. Only add the letters A, E, I, O, or Y. Do not add any additional consonants or other characters in the corrections.
 Input: frnd
 Output: [ friend frond ]
 Input: mmr
@@ -39,8 +41,12 @@ Input: fld
 Output: [ afield failed field filed flayed fled flood fluid foaled fold ]
 Input: ${input}
 Output: [`,
-    stop: [' ]'],
-  });
+      stop: [' ]'],
+    });
+  } catch (e) {
+    console.error(e);
+    return reply.send("error talking to OpenAI!")
+  }
 
   const cost = (pricing[model] * completion.data.usage.total_tokens) / 1000;
   accumulatedCost += cost;
